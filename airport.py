79573c3bad76c70eba23b638c16efbccb9cc7818
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-11-17 18:39:44 +0100

    update runway and airport class parameters

--- airport.py
+++ airport.py
@@ -1,10 +1,11 @@
 from runway import Runway
 
+
 class Airport:
 
-    def __init__(self, width=10000, length=10000, height=5000):
-        self.width = width
-        self.length = length
-        self.height = height
-        self.runway_alfa = Runway()
-        self.runway_beta = Runway()
+    def __init__(self, width_area=10000, length_area=10000, height_area=5000):
+        self.width_area = width_area
+        self.length_area = length_area
+        self.height_area = height_area
+        self.runway_alfa = Runway(start_point_width=0, start_point_length=2000)
+        self.runway_beta = Runway(start_point_width=0, start_point_length=8000)
--- runway.py
+++ runway.py
@@ -1,6 +1,15 @@
 class Runway:
 
-    def __init__(self, width=80, length=3000):
+    def __init__(self, start_point_width, start_point_length, width=80, length=3000):
         self.width = width
         self.length = length
-
+        self.start_point = {
+            "width": start_point_width,
+            "length": start_point_length,
+            "height": 0
+        }
+        self.end_point = {
+            "width": start_point_width,
+            "length": start_point_length + length,
+            "height": 0
+        }
