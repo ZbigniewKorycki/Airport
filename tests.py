12c47cd6e4e1d2aafa08af1f835544d5a717df16
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-11-24 16:27:11 +0100

    update test_get_starting_position

--- tests.py
+++ tests.py
@@ -12,14 +12,11 @@ class TestAirplane(unittest.TestCase):
 
     def test_get_starting_position(self):
         starting_position = self.airplane.get_starting_position()
-        self.assertIn("x", starting_position)
-        self.assertIn("y", starting_position)
-        self.assertIn("z", starting_position)
         self.assertEqual(3, len(starting_position))
-        self.assertGreaterEqual(5000, starting_position["z"])
-        self.assertLessEqual(2000, starting_position["z"])
-        self.assertGreaterEqual(10000, starting_position["x"])
-        self.assertLessEqual(0, starting_position["x"])
-        self.assertGreaterEqual(10000, starting_position["y"])
-        self.assertLessEqual(0, starting_position["y"])
+        self.assertGreaterEqual(5000, starting_position[2])
+        self.assertLessEqual(2000, starting_position[2])
+        self.assertGreaterEqual(10000, starting_position[0])
+        self.assertLessEqual(0, starting_position[0])
+        self.assertGreaterEqual(10000, starting_position[1])
+        self.assertLessEqual(0, starting_position[1])
 
