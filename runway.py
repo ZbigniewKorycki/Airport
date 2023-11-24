aca5c8ef4fad6b4c58384108a4dd81eafa07c4b6
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-11-24 16:27:38 +0100

    change attribute of runway for more meaningful

--- runway.py
+++ runway.py
@@ -7,7 +7,7 @@ class Runway:
 
     def __init__(self, start_coordinate_x, start_coordinate_y, length=2000):
         self.length = length
-        self.start_point_localisation = np.array([start_coordinate_x, start_coordinate_y, 0])
+        self.entry_point_localisation = np.array([start_coordinate_x, start_coordinate_y, 0])
         self.end_point_localisation = np.array([start_coordinate_x, start_coordinate_y + length, 0])
         self.is_free = True
 
